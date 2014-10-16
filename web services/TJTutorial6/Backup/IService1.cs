1734f3c23f3917fcaa387fba0aeda180a491e913
sbbb2 <sbbb2@mail.umkc.edu>
2014-10-15 23:54:10 -0500

    First Increment

---
+++ web services/TJTutorial6/Backup/IService1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TJTutorial6
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetMeanValue(int value, int value2);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        string AuthenticateUser(string userid, string password);
+
+        //[OperationContract]
+        //string CreateRequest(string userid, string description, string location);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+        string somevalue = "eg";        
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ web services/TJTutorial6/Backup/Service1.svc.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data.OleDb;
+
+
+namespace TJTutorial6
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Service1 : IService1
+    {
+        public string GetMeanValue(int value, int value2)
+        {
+                      
+            return ((value + value2) / 2).ToString();
+            //return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string AuthenticateUser(string userid, string password)
+        {
+            OleDbConnection PubsConn = new OleDbConnection("Provider=SQLOLEDB;Data Source=.\\SQLEXPRESS;" + "integrated Security=sspi;initial catalog=iVolunteer;");
+            OleDbCommand testCMD = new OleDbCommand("AuthenticateUser", PubsConn);
+
+            testCMD.CommandType = CommandType.StoredProcedure;
+            string status = String.Empty;
+            
+            OleDbParameter IdIn = testCMD.Parameters.Add
+               ("@Userid", OleDbType.VarChar, 20);
+            IdIn.Direction = ParameterDirection.Input;
+            OleDbParameter NumTitles = testCMD.Parameters.Add
+               ("@password", OleDbType.VarChar, 20);
+            NumTitles.Direction = ParameterDirection.Input;
+
+            IdIn.Value = userid;
+            NumTitles.Value = password;
+
+            PubsConn.Open();
+
+            OleDbDataReader myReader = testCMD.ExecuteReader();
+            Console.WriteLine("Book Titles for this Author:");
+            while (myReader.Read())
+            {
+                status = myReader.GetString(0);
+            };
+            myReader.Close();
+
+            return status;
+            
+
+
+        }
+    }
+}
